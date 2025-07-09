6938bc882467c9f7a07b14c6665730011f2ae133
Luan Ferreira <luanhsouzaf@gmail.com>
2025-07-08 21:03:07 -0300

    chore: Initial project setup from Spring Initializr

---
+++ src/test/java/br/com/luanferreira/desafio/gerenciador_clientes_api/GerenciadorClientesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.luanferreira.desafio.gerenciador_clientes_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorClientesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
