20ebecdc8c5fb1d6893d9ff2721243b83229b6a5
Luan Ferreira <luanhsouzaf@gmail.com>
2025-07-15 09:32:28 -0300

    fix: import environment in cep service

--- frontend/src/app/shared/services/cep.service.ts
+++ frontend/src/app/shared/services/cep.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { ViaCepResponse, EnderecoRequest } from '../models/endereco.model';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
